05072898d33d189855ab19a1cad10c37d0d19b1f
Bharati <bharatishirsath51@gmail.com>
2022-09-16 17:15:40 +0530

    Second commit

--- src/sourcePack/Firstcode.java
+++ src/sourcePack/Firstcode.java
@@ -4,7 +4,7 @@ public class Firstcode {
 
 	public static void main(String[] args) {
 	String str="BharatiSase";
-	System.out.println(str.substring(0, 7)+" "+str.substring(7,str.length()));
+	System.out.println(str.substring(0, 7)+" "+str.substring(str.length()-4,str.length()));
 
 	}
 
